88c5ffc13aa2835e63a1b1a74c917816141a60a9
Tribeoftech <gratefulgrowing333@gmail.com>
2024-06-08 12:35:42 -0500

    creating and adding provided main.dart files

---
+++ 0x01-dart-function/0-hellofunc.dart
@@ -0,0 +1,4 @@
+/// Prints a greeting message with the provided name.
+void helloFunction(String name) {
+  print("Hello ${name} from dart");
+}
---
+++ 0x01-dart-function/5-main.dart
@@ -0,0 +1,7 @@
+import '5-calculate-area.dart';
+
+main() {
+  print(calculateArea(30,20));
+  print(calculateArea(2.4,3.5));
+  print(calculateArea(8.5,3.7));
+}
