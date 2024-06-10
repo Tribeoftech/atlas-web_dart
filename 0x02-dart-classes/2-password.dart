459ac0b6a1939ba5e19c3003d59cccaa45f90da4
Tribeoftech <gratefulgrowing333@gmail.com>
2024-06-10 15:03:12 -0500

    task2 create usr & pas class

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,49 @@
+// Create a User and Password class.
+class User {
+  // properties
+  String name = "";
+  int age = 0;
+  double height = 0.0;
+
+  // constructor
+  User({required this.name, required this.age, required this.height});
+
+  // methods
+  String showName() {
+    return "Hello $name";
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+      'height': height
+    };
+  }
+}
+
+
+class Password {
+  String password = "";
+
+  Password() {
+    this.password = password;
+  }
+
+  bool isValid() {
+   if (
+     ((password.length >= 8) && (password.length <= 16))
+    && (password.contains(new RegExp(r'[A-Z]')))
+    && (password.contains(new RegExp(r'[a-z]')))
+    && (password.contains(new RegExp(r'[0-9]')))) {
+     return true;
+   } else {
+     return false;
+   }
+  }
+
+ @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
