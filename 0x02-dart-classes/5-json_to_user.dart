e16369965cda69ddc8a9791e095cfad4ad353526
Tribeoftech <gratefulgrowing333@gmail.com>
2024-06-10 21:28:05 -0500

    rework task 5

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,30 +1,18 @@
-// User class that has name as String, age as Integer, and height as double.
 class User {
-  // properties
-  int id = 0;
-  String name = "";
-  int age = 0;
-  double height = 0.0;
+  // Properties
+  String name;
+  int age;
+  double height;
+  int id;
 
-  // constructor
-  User({required this.id, required this.name, required this.age, required this.height});
+  // Constructor
+  User({this.name = '', this.age = 0, this.height = 0.0, this.id = 0});
 
-  // methods
+  // Methods
   String showName() {
-    return "Hello $name";
+    return 'Hello $name';
   }
 
-  // instance method fromJson()
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-    );
-  }
-
-  // instance method toJson()
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -34,43 +22,16 @@ class User {
     };
   }
 
-  // instance method toString()
-  @override
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
   String toString() {
     return 'User(id : $id ,name: $name,  age: $age, height: $height)';
-  }
-}
-
-
-class Password {
-  var _password = "";
-
-  // constructor
-  Password({required String password}) : _password = password;
-
-  // setter
-  set password(String password) {
-    _password = password;
-  }
-
-  // getter
-  String get password => _password;
-
-  // isValid() setter
-  bool isValid() {
-   if (
-     ((password.length >= 8) && (password.length <= 16))
-    && (password.contains(new RegExp(r'[A-Z]')))
-    && (password.contains(new RegExp(r'[a-z]')))
-    && (password.contains(new RegExp(r'[0-9]')))) {
-     return true;
-   } else {
-     return false;
-   }
-  }
-
- @override
-  String toString() {
-    return 'Your Password is: $password';
   }
 }
