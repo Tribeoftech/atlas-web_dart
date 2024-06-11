d5f3bb4e0fbd1126054c7d1ea34147b2a0c18b52
Tribeoftech <gratefulgrowing333@gmail.com>
2024-06-11 13:02:04 -0500

    reworking task1-2

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,10 +1,13 @@
 import '1-main.dart';
 import 'dart:convert';
-// getUserID() returns a Future string representing the user id
 
-
+// getUserId() returns a Future string representing the user id
 Future<String> getUserId() async {
-  final String userData = await fetchUserData();
-  final Map<String, dynamic> user = jsonDecode(userData);
-  return user['id'];
+  try {
+    final String userData = await fetchUserData();
+    final Map<String, dynamic> user = jsonDecode(userData);
+    return user['id'];
+  } catch (e) {
+    return 'error caught: $e';
+  }
 }
--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,18 +1,8 @@
 import '2-main.dart';
-
-// fetchUser function returns the user's ID as a string
-// utilizes try-catch block to handle errors
-Future<String> fetchUser() async {
-  try {
-    final Map<String, dynamic> user = await json.decode(await fetchUserData());
-    return user['id'];
-  } catch (e) {
-    return 'error caught: $e';
-  }
-}
-
 // getUser function prints user's string representation
 // utilizes try-catch block to handle errors
+
+
 Future<void> getUser() async {
   try {
     print(await fetchUser());
